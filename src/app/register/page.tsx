6d3a961776897a7c30846043983bf4cb7c642b33
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-27 03:20:41 +0600

    "working on register page"

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,11 +1,26 @@
-
+import { Box, Container, Stack } from "@mui/material";
 
 const RegisterPage = () => {
-    return (
-        <div>
-            <h1>Register here man</h1>
-        </div>
-    );
+  return (
+    <Container>
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            p: 4,
+          }}
+        ></Box>
+      </Stack>
+    </Container>
+  );
 };
 
 export default RegisterPage;
