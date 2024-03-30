c51b547468fc0812645c101d8194cd79a236f02d
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-30 07:57:49 +0600

    "added custom axios fetcher for rtk query"

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,13 +1,14 @@
+import { axiosBaseQuery } from '@/helpers/axios/axiosBaseQuery'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // //import type { Pokemon } from './types'
 
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/v1' }),
+    baseQuery: axiosBaseQuery({ baseUrl: 'http://localhost:5000/api/v1' }),
     endpoints: () => ({}),
 })
 
 // // Export hooks for usage in functional components, which are
 // // auto-generated based on the defined endpoints
-// // export const { useGetPokemonByNameQuery } = pokemonApi
+// // export const { useGetPokemonByNameQuery } = pokemonApi 
